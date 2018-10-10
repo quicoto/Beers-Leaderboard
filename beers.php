de60f5ca4b42ff02a956b285e66ad4b717e6bd06
Ricard Torres <ricard.torres@netcentric.biz>
2018-10-10 12:25:33 +0200

    Name of thre brew in the 3 lists

--- beers.php
+++ beers.php
@@ -133,8 +133,7 @@ function beers_shortcode() {
     foreach ( $beers as $post ) : setup_postdata( $post );
       echo '<li>';
         $brew = wp_get_post_terms( $post->ID, 'brew' );
-
-        echo '<a href="' . get_the_permalink($post). '">' . $brew[0]->name . '</a>';
+        echo $brew[0]->name;
 
         $score = get_post_meta( $post->ID, 'rating_score', true );
         if (!$score) {
@@ -184,7 +183,8 @@ function beers_shortcode() {
   echo "<ul>";
     foreach ( $beers as $post ) : setup_postdata( $post );
       echo '<li>';
-        echo '<a href="' . get_the_permalink($post). '">' . get_the_title($post) . '</a>';
+        $brew = wp_get_post_terms( $post->ID, 'brew' );
+        echo $brew[0]->name;
 
         $score = get_post_meta( $post->ID, 'rating_score', true );
         if (!$score) {
@@ -233,7 +233,8 @@ function beers_shortcode() {
   echo "<ul>";
     foreach ( $beers as $post ) : setup_postdata( $post );
       echo '<li>';
-        echo '<a href="' . get_the_permalink($post). '">' . get_the_title($post) . '</a>';
+        $brew = wp_get_post_terms( $post->ID, 'brew' );
+        echo $brew[0]->name;
 
         $score = get_post_meta( $post->ID, 'rating_score', true );
         if (!$score) {
